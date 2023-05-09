edb229e215a90823ee82dc91d8e0d123838ad0d2
전현진 <jeonhyeonjin@jeonhyeonjin-ui-MacBookPro.local>
2023-05-09 12:16:51 +0900

    feat: cor, helmet 설정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
 import { setupSwagger } from './global/swagger';
+import helmet from 'helmet';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
 
   setupSwagger(app);
+
+  app.enableCors();
+  app.use(helmet({ contentSecurityPolicy: false }));
 
   await app.listen(3000);
 }
