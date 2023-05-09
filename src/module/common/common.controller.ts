f58023fec299469e00c2815b64ee3a84da0084cf
Junnnnnnnnnnn <wjsguswls71@naver.com>
2023-05-09 17:04:58 +0900

    test: commit test

--- src/module/common/common.controller.ts
+++ src/module/common/common.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { ConfigService } from 'src/config/config.service';
-import { Logger } from 'src/log/logger';
 @ApiTags('common')
 @Controller()
 export class CommonController {
@@ -9,9 +7,6 @@ export class CommonController {
 
   @Get('healthcheck')
   public healthcheck(): string {
-    Logger.info({
-      message: `App started on env ${ConfigService.getConfig().ENV}, port ${ConfigService.getConfig().PORT}`,
-    }, 'App');
     return 'OK';
   }
 }   
